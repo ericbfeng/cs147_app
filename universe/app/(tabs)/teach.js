707d6fc8defd22354240922025fd7710dcb1de25
sallyw02 <167825335+sallyw02@users.noreply.github.com>
2024-11-16 17:56:27 -0800

    4 Tabs created + changed files to js
    We can each start working on our tabs and make nested folders as necessary

---
+++ universe/app/(tabs)/teach.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Teach() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hi</Text>
+    </View>
+  );
+}
