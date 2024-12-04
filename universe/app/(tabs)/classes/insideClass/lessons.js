d2d2b600f7fafd773930b7341a7d19c71e639796
stevedough123 <steve.dou@gmail.com>
2024-12-04 13:02:59 -0800

    Fixing data for demo.

--- universe/app/(tabs)/classes/insideClass/lessons.js
+++ universe/app/(tabs)/classes/insideClass/lessons.js
@@ -36,12 +36,14 @@ export default function LessonsScreen() {
   }
 
   const handleLessonPress = (item) => {
-    
+    navigation.navigate("specificLesson", { data: item });
+    /*
     console.log("HELLO ITEM ", item);
     router.push({
       pathname: "classes/insideClass/specificLesson",
       params: { data: item },
     });
+    */
   };
 
   const renderItem = ({ item }) => (
