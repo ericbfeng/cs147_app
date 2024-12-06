c412f79b1bd8f759fa2834814948e9c369c83e0c
stevedough123 <steve.dou@gmail.com>
2024-12-06 00:00:18 -0800

    Saving before navigation.

--- universe/app/(tabs)/classes/insideClass/students.js
+++ universe/app/(tabs)/classes/insideClass/students.js
@@ -41,7 +41,7 @@ export default function StudentsScreen() {
   }
 
   const handleNewStudent = () => {
-    console.log("test");
+    navigation.navigate("newStudentModal");
   };
 
   const handleDelete = (id) => {
